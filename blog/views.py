5942c2e4f962ffd3e6eda75811369c4a39164de2
Argen <turdalievargen@gmail.com>
2022-07-27 12:53:46 +0600

    crud posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
+
 from rest_framework.response import Response
 
+from django.shortcuts import get_object_or_404
 from .serializers import PostSerializer
 from .models import Post
 
@@ -10,3 +12,33 @@ def posts_list(request):
     serializer = PostSerializer(queryset, many=True)
     return Response(serializer.data)
     
+@api_view(['POST'])
+def create_post(request):
+    data = request.POST
+    serializer = PostSerializer(data=request.POST)
+    if serializer.is_valid(raise_exception= True):
+        serializer.save()
+        return Response('Пост успешно создан')
+
+@api_view(['GET'])
+def post_detail(request, p_id):
+    post = get_object_or_404(Post, id=p_id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def post_delete(request, p_id):
+    post = get_object_or_404(Post, id=p_id)
+    post.delete()
+    return Response('Пост успешно удален')
+
+@api_view(['PUT', 'PATCH'])
+def post_update(request, p_id):
+    post = get_object_or_404(Post, id=p_id)
+    serializer = PostSerializer(instance = post, data = request.POST)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+    return Response('Пост успешно обнавлен')
+
+
+
